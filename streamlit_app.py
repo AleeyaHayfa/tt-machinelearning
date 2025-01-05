9c414449520f9d73bfd108a575102a3325740698
AleeyaHayfa <121542712+AleeyaHayfa@users.noreply.github.com>
2025-01-05 12:07:55 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,16 @@ st.title('ThinkTankers ML App')
 st.write('This app builds a machine learning model')
 
 # nanti boleh tukar dataset kat sini
-with st.expander('Data'):
+with st.expander('Data'): #expander tu drop down button
   st.write('**Raw Data**')
   df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv")
   df
+
+  st.write('**X**')
+  X = df.drop('species', axis=1)
+  X
+
+  st.write('Y')
+  Y = df.species
+  Y
+ 
