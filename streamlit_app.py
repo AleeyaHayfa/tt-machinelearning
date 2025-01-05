338b36a407615e0c75503b1286bb6cadec642437
AleeyaHayfa <121542712+AleeyaHayfa@users.noreply.github.com>
2025-01-05 12:26:11 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ with st.sidebar:
   gender = st.selectbox('Gender', ('male', 'female'))
   # 32.1 mininum, 59.6 maximum, 43.9 average (same as other)
   bill_length_mm = st.slider('Bill length(mm)', 32.1, 59.6, 43.9)
-  bill_length_mm = st.slider('Bill depth(mm)', 13.1, 21.5, 17.2)
+  bill_depth_mm = st.slider('Bill depth(mm)', 13.1, 21.5, 17.2)
   flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
   body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0) 
   #"species","island","bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g","sex"
