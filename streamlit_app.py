6652222453a4b64caf5e6e8834e4f083bc05743b
AleeyaHayfa <121542712+AleeyaHayfa@users.noreply.github.com>
2025-01-05 12:13:33 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title('ThinkTankers ML App')
 
 st.write('This app builds a machine learning model')
 
-# nanti boleh tukar dataset kat sini
+# nanti boleh tukar clean letak kat sini
 with st.expander('Data'): #expander tu drop down button
   st.write('**Raw Data**')
   df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv")
@@ -21,3 +21,8 @@ with st.expander('Data'): #expander tu drop down button
 
 with st.expander('Data Visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+
+# Data prepration
+with st.sidebar:
+  st.header('Input features')
+  
