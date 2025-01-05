ebe52ac3ee6472e501700f041ebef85f594b103c
AleeyaHayfa <121542712+AleeyaHayfa@users.noreply.github.com>
2025-01-05 12:04:35 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.title('ThinkTankers ML App')
 st.write('This app builds a machine learning model')
 
 # nanti boleh tukar dataset kat sini
-with st.expander('Data');
+with st.expander('Data'):
   st.write('**Raw Data**')
   df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv")
   df
