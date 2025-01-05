58d87c7452814f13d52afd444d335ece93fd08a7
AleeyaHayfa <121542712+AleeyaHayfa@users.noreply.github.com>
2025-01-05 12:01:26 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('ThinkTankers ML App')
 
 st.write('This app builds a machine learning model')
+
+df = pd.read_csv("https://github.com/dataprofessor/data/blob/master/penguins_cleaned.csv")
+df
